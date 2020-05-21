249cdefdb6c7c83e3664c00b6b69b4c43dd672c4
ParameshRD <parameshrdgowda97@gmail.com>
2020-05-21 19:00:15 +0530

    first commit

---
+++ src/main/java/com/example/devopscasestudydevopsdockerkube/DevopscasestudyDevopsDockerKubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopscasestudydevopsdockerkube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopscasestudyDevopsDockerKubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopscasestudyDevopsDockerKubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopscasestudydevopsdockerkube/DevopscasestudyDevopsDockerKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopscasestudydevopsdockerkube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopscasestudyDevopsDockerKubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
